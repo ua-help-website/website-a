30e76c0330896211cc1b14e4b9bd51592f98c990
Matthias Wünsch <matthias.wuensch175@gmail.com>
2022-03-01 15:53:15 +0100

    remote backup script

--- server-config/backup/backup.sh
+++ server-config/backup/backup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-backupLocation="/srv/backup/"
-dataLocation="ua-help-a_db_data"
+backupLocation="/srv/backup"
+
+dockerDataLocation="ua-help-a_db_data"
+wordpressDataLocation="/srv/www/wp-content"
 
 docker run --rm \
     -v $backupLocation:/backup \
-    -v $dataLocation:/data:ro \
+    -v $dockerDataLocation:/data:ro \
     debian:stretch-slim bash -c "cd /data && /bin/tar -czvf /backup/ua-help.tar.gz ."
+
+tar -czvf $backupLocation/wp-backup.tar $wordpressDataLocation
